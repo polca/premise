504f8b2d06f45d0825dccaea161df94b822f6046
Alois Dirnaichner <alodi@directbox.com>
2018-11-28 17:01:31 +0100

    Collect activitymaps in a python module.

--- notebooks/helpers/ei2rmnd.py
+++ notebooks/helpers/ei2rmnd.py
@@ -2,6 +2,7 @@
 
 import brightway2 as bw
 from helpers.eimod import geomatcher
+from helpers import activitymaps
 
 def remove_double_counting(lca, activities_of_interest, all_activities):
     """Modify the LCA inventory by excluding ``all_activities`` that are not
@@ -44,7 +45,11 @@ def multi_lca_average(actvts, demand=1.):
 
 
 def find_activities_by_name(techname, db):
-    return [act for act in db if act["name"] == techname]
+    """Provide string or list of strings to search for activities (exact matches)."""
+    if type(techname) == list:
+        return [act for act in db if act["name"] in techname]
+    else:
+        return [act for act in db if act["name"] == techname]
 
 
 def find_activities_in_regions(techname, regions, db):
@@ -148,93 +153,6 @@ def get_REMIND_database_name(scenario, year):
     return "_".join(["ecoinvent", "Remind", scenario, str(year)])
 
 
-def act_fltr(db, fltr={}, mask={}, filter_exact=False, mask_exact=False):
-    """Filter `db` for activities matching field contents given by `fltr` excluding strings in `mask`.
-
-    Args:
-      `db`: brightway database object.
-      `fltr`: string, list of strings or dictionary.
-        If a string is provided, it is used to match the name field from the start (*startswith*).
-        If a list is provided, all strings in the lists are used and results are joined (*or*).
-        A dict can be given in the form <fieldname>: <str> to filter for <str> in <fieldname>.
-      `mask`: used in the same way as `fltr`, but filters add up with each other (*and*).
-      `filter_exact` and `mask_exact`: boolean, set `True` to only allow for exact matches.
-
-    Returns:
-      list of brightway activities
-    """
-    result = []
-
-    # default field is name
-    if type(fltr) == list or type(fltr) == str:
-        fltr = {
-            "name": fltr
-        }
-    if type(mask) == list or type(mask) == str:
-        mask = {
-            "name": mask
-        }
-
-    def like(a, b):
-        if filter_exact:
-            return a == b
-        else:
-            return a.startswith(b)
-
-    def notlike(a, b):
-        if mask_exact:
-            return a != b
-        else:
-            return b not in a
-
-    assert len(fltr) > 0, "Filter dict must not be empty."
-    for field in fltr:
-        condition = fltr[field]
-        if type(condition) == list:
-            for el in condition:
-                # this is effectively connecting the statements by *or*
-                result.extend([act for act in db if like(act[field], el)])
-        else:
-            result.extend([act for act in db if like(act[field], condition)])
-
-    for field in mask:
-        condition = mask[field]
-        if type(condition) == list:
-            for el in condition:
-                # this is effectively connecting the statements by *and*
-                result = [act for act in result if notlike(act[field], el)]
-        else:
-            result = [act for act in result if notlike(act[field], condition)]
-    return result
-
-
-techno_filters = {
-    "steel": {
-        "fltr": "market for steel,",
-        "mask": "hot rolled"},
-    "concrete": {"fltr": "market for concrete,"},
-    "copper": {
-        "fltr": "market for copper",
-        "filter_exact": True},
-    "aluminium": {
-        "fltr": ["market for aluminium, primary",
-                 "market for aluminium alloy,"]},
-    "electricity": {"fltr": "market for electricity"},
-    "gas": {
-        "fltr": "market for natural gas,",
-        "mask": ["network", "burned"]},
-    "diesel": {
-        "fltr": "market for diesel",
-        "mask": ["burned", "electric"]},
-    "petrol": {
-        "fltr": "market for petrol,",
-        "mask": "burned"},
-    "freight": {"fltr": "market for transport, freight"},
-    "cement": {"fltr": "market for cement,"},
-    "heat": {"fltr": "market for heat,"}
-}
-
-
 def add_REMIND_technosphere_flows(reset_flows=False):
     """Add material flows within ecoinvent as biosphere endpoints to account
     for these flows in REMIND scenarios.
@@ -250,7 +168,7 @@ def add_REMIND_technosphere_flows(reset_flows=False):
             eidb = bw.Database(db)
 
             techno_markets = {
-                tech: act_fltr(eidb, **conditions) for tech, conditions in techno_filters.items()}
+                tech: find_activities_by_name(actnames, eidb) for tech, actnames in activitymaps.materials.items()}
 
             print("Check for consistent units across technologies.")
             for kind, actlst in techno_markets.items():
