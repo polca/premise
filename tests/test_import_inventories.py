7a82b2a133c29b10c68b1351575d6ad536edd8b8
Alois Dirnaichner <alodi@directbox.com>
2019-10-16 16:46:06 +0200

    Re-add biosphere tests.

---
+++ tests/test_import_inventories.py
@@ -0,0 +1,61 @@
+# content of test_activity_maps.py
+import pytest
+from rmnd_lca.inventory_imports import BaseInventoryImport
+from pathlib import Path
+
+
+def get_db():
+    db = [{
+            'name':'fake activity',
+            'reference product': 'fake product',
+            'location':'IAI Area, Africa',
+            'unit':'kilogram',
+            'exchanges': [
+                {'name' : 'fake activity',
+                 'product': 'fake product',
+                 'amount': 1,
+                 'type': 'production',
+                 'unit':'kilogram',
+                 'input':('dummy_db', '6543541'),},
+                {'name' : '1,4-Butanediol',
+                 'categories': ('air', 'urban air close to ground'),
+                 'amount': 1,
+                 'type': 'biosphere',
+                 'unit':'kilogram',
+                 'input':('dummy_bio', '123'),
+                },
+            ]
+        }]
+    version = 3.5
+    return db, version
+
+
+def test_biosphere_dict():
+    db, version = get_db()
+    testpath = Path("testfile")
+    open(testpath, "w")
+    dbc = BaseInventoryImport(db, version, testpath)
+    assert dbc.biosphere_dict[
+               (
+                   '1,4-Butanediol',
+                   'air',
+                   'urban air close to ground',
+                   'kilogram'
+               )] == '38a622c6-f086-4763-a952-7c6b3b1c42ba'
+
+
+def test_biosphere_dict_2():
+    db, version = get_db()
+    testpath = Path("testfile")
+    open(testpath, "w")
+    dbc = BaseInventoryImport(db, version, testpath)
+
+    for act in dbc.db:
+        for exc in act['exchanges']:
+            if exc['type'] == 'biosphere':
+                assert dbc.biosphere_dict[(
+                    exc['name'],
+                    exc['categories'][0],
+                    exc['categories'][1],
+                    exc['unit']
+                )] == '38a622c6-f086-4763-a952-7c6b3b1c42ba'
