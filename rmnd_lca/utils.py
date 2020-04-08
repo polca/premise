ffd8f66a9becd106e3917c9dff5bc941dc44c82d
romainsacchi <r_s@me.com>
2020-04-08 17:35:36 +0200

    Code refactoring

--- rmnd_lca/utils.py
+++ rmnd_lca/utils.py
@@ -8,9 +8,25 @@ LHV_FUELS = DATA_DIR / "fuels_lower_heating_value.txt"
 CLINKER_RATIO_ECOINVENT_36 = DATA_DIR / "cement" / "clinker_ratio_ecoinvent_36.csv"
 CLINKER_RATIO_ECOINVENT_35 = DATA_DIR / "cement" / "clinker_ratio_ecoinvent_35.csv"
 CLINKER_RATIO_REMIND = DATA_DIR / "cement" / "clinker_ratios.csv"
+REMIND_TO_FUELS = DATA_DIR / "steel" / "remind_fuels_correspondance.txt"
 
 def eidb_label(scenario, year):
     return "ecoinvent_" + scenario + "_" + str(year)
+
+def get_correspondance_remind_to_fuels():
+    """
+    Return a dictionary with REMIND fuels as keys and ecoinvent activity names and reference products as values.
+    :return: dict
+    :rtype: dict
+    """
+    d = {}
+    with open(REMIND_TO_FUELS) as f:
+        r = csv.reader(f, delimiter=";")
+        for row in r:
+            d[row[0]] = {"fuel name": row[1], "activity name": row[2], "reference product": row[3]}
+    return d
+
+
 
 def get_fuel_co2_emission_factors():
     """
