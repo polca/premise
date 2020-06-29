3fca647f2c00e83cf0262ca8bfcf1d2d76f0ef52
Alois Dirnaichner <alodi@directbox.com>
2020-06-29 15:06:26 +0200

    Skip ecoinvent-dependent tests on travis.

--- tests/test_cars.py
+++ tests/test_cars.py
@@ -1,14 +1,17 @@
 from rmnd_lca import NewDatabase
+from rmnd_lca import RemindDataCollection
 from rmnd_lca.cars import Cars
 
+import pytest
 import wurst
 import brightway2 as bw
 
 bw.projects.set_current("transport_lca")
-scenario="SSP2-PkBudg900"
-year=2050
+scenario = "SSP2-PkBudg900"
+year = 2050
 
 
+@pytest.mark.ecoinvent
 def test_create_local_evs():
     ndb = NewDatabase(
         scenario=scenario,
@@ -20,6 +23,7 @@ def test_create_local_evs():
     Cars(ndb.db, ndb.rdc, scenario, year).create_local_evs()
 
 
+@pytest.mark.ecoinvent
 def test_create_local_fcevs():
     ndb = NewDatabase(
         scenario=scenario,
@@ -31,6 +35,7 @@ def test_create_local_fcevs():
     Cars(ndb.db, ndb.rdc, scenario, year).create_local_fcevs()
 
 
+@pytest.mark.ecoinvent
 def test_full_import():
     ndb = NewDatabase(
         scenario=scenario,
@@ -50,11 +55,17 @@ def test_full_import():
     # del bw.datases[dbname]
     return ndb.db
 
+
 def test_get_fuel_mix():
-    data = ndb.rdc.get_remind_fuel_mix()
+    from rmnd_lca import DATA_DIR
+    remind_file_path = DATA_DIR / "remind_output_files"
+    rdc = RemindDataCollection(scenario, year, remind_file_path)
+    data = rdc.get_remind_fuel_mix()
     assert data.shape == (13, 3)
     assert all(data.sum(dim="variables") == 1.)
 
+
+@pytest.mark.ecoinvent
 def test_update_fuel_mix():
     ndb = NewDatabase(
         scenario=scenario,
