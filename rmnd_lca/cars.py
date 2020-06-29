1137ab2a13ee709ed4b8bde70d169a7ed8e3f1dc
Alois Dirnaichner <alodi@directbox.com>
2020-06-29 12:46:04 +0200

    Update carculator inventories according REMIND fuel mix
    - for bioliquids, single, globally homogenous activities are
    applied. This has to be implemented still.
    - there is no syngas at the moment.

--- rmnd_lca/cars.py
+++ rmnd_lca/cars.py
@@ -157,31 +157,13 @@ class Cars():
             wurst.delete_zero_amount_exchanges([supply])
 
             # find hydrogen supply nearby
-            ei_locs = self.geo.remind_to_ecoinvent_location(region)
             h2sups = ws.technosphere(
                 supply,
                 ws.startswith("product", "Hydrogen"))
             for h2sup in h2sups:
-                producers = list(ws.get_many(
-                    self.db,
-                    ws.equals("name", h2sup["name"]),
-                    ws.either(*[
-                        ws.equals("location", loc) for loc in ei_locs
-                    ])))
-                if len(producers) >= 1:
-                    prod = producers[0]
-                    if len(producers) > 1:
-                        print(("Multiple producers for {} found in {}, "
-                               "using hydrogen from {}").format(
-                                   h2sup["product"], region, prod["location"]))
-                    h2sup["location"] = prod["location"]
-                    h2sup["name"] = prod["name"]
-                else:
-                    # we can leave things as they are since the existing
-                    # supply is the default supply
-                    print(("No producers for {} found in {}, "
-                           "using hydrogen from {}").format(
-                               h2sup["product"], region, h2sup["location"]))
+                prod = self._find_local_supplier(region, h2sup["name"])
+                h2sup["location"] = prod["location"]
+                h2sup["name"] = prod["name"]
 
             # create local fcev
             for fcev in fcevs:
@@ -195,14 +177,179 @@ class Cars():
                 self.db.append(local_fcev)
             self.db.append(supply)
 
-    def update_fuel_mix(self):
+    def _find_local_supplier(self, region, name):
+        """
+        Use geomatcher to find a supplier with `name` first strictly
+        within the region, then in an intersecting region and
+        eventually *any* activity with this name.
+        """
+        def producer_in_locations(locs):
+            prod = None
+            producers = list(ws.get_many(
+                self.db,
+                ws.equals("name", name),
+                ws.either(*[
+                    ws.equals("location", loc) for loc in locs
+                ])))
+            if len(producers) >= 1:
+                prod = producers[0]
+                if len(producers) > 1:
+                    print(("Multiple producers for {} found in {}, "
+                           "using activity from {}").format(
+                               name, region, prod["location"]))
+            return prod
+
+        ei_locs = self.geo.remind_to_ecoinvent_location(region, contained=True)
+        prod = producer_in_locations(ei_locs)
+        if prod is None:
+            ei_locs = self.geo.remind_to_ecoinvent_location(region)
+            prod = producer_in_locations(ei_locs)
+            if prod is None:
+                # let's use "any" dataset
+                producers = list(ws.get_many(
+                    self.db,
+                    ws.equals("name", name)))
+                if len(producers) == 0:
+                    raise ValueError("No producers found for {}.")
+                prod = producers[0]
+                # we can leave things as they are since the existing
+                # supply is the default supply
+                print(("No producers for {} found in {}\n"
+                       "Using activity from {}")
+                      .format(name, region, prod["location"]))
+        return prod
+
+    def create_local_icevs(self):
+        """
+        Use REMIND fuel markets to update the mix of bio-, syn-
+        and fossil liquids in gasoline and diesel.
+        """
+        icevs = list(ws.get_many(
+            self.db,
+            ws.either(
+                ws.contains("name", "ICEV-"),
+                ws.contains("name", "HEV-"))
+            ))
+
+        old_suppliers = {
+            fuel: ws.get_one(
+                self.db,
+                ws.startswith(
+                    "name", "fuel supply for {} vehicles".format(fuel)))
+            for fuel in ["diesel", "gasoline"]}
+
+        new_producers = {
+            "diesel": {
+                # biodiesel is only from cooking oil from RER,
+                # as this is not the focus for now
+                # to be improved!
+                "Biomass": ws.get_one(
+                    self.db,
+                    ws.equals("name", "Biodiesel from cooking oil"))
+            },
+            "gasoline": {
+                # only ethanol from European wheat straw as biofuel
+                "Biomass": ws.get_one(
+                    self.db,
+                    ws.equals("name", "Ethanol from wheat straw pellets"),
+                    ws.equals("location", "RER")),
+                "Hydrogen": ws.get_one(
+                    self.db,
+                    ws.equals("name",
+                              "Gasoline production, synthetic, from methanol"),
+                    ws.equals("location", "RER"))
+            }
+        }
+
         data = self.rmd.get_remind_fuel_mix()
         for region in self.remind_regions:
-            # find activities
-            icevs = ws.get_many(
-                self.db,
-                ws.either(
-                    ws.contains("name", "ICEV-"),
-                    ws.contains("name", "HEV-")),
-                ws.equals("location", region))
+            # two regions for gasoline and diesel production
+            if region == "EUR":
+                new_producers["gasoline"]["Fossil"] = ws.get_one(
+                    self.db,
+                    ws.equals("name", "market for petrol, low-sulfur"),
+                    ws.equals("location", "Europe without Switzerland"))
+                new_producers["diesel"]["Fossil"] = ws.get_one(
+                    self.db,
+                    ws.equals("name", "market group for diesel"),
+                    ws.equals("location", "RER"))
+            else:
+                new_producers["gasoline"]["Fossil"] = ws.get_one(
+                    self.db,
+                    ws.equals("name", "market for petrol, low-sulfur"),
+                    ws.equals("location", "RoW"))
+                new_producers["diesel"]["Fossil"] = ws.get_one(
+                    self.db,
+                    ws.equals("name", "market group for diesel"),
+                    ws.equals("location", "GLO"))
 
+            # local syndiesel
+            new_producers["diesel"]["Hydrogen"] = self._find_local_supplier(
+                region, "Diesel production, Fischer Tropsch process")
+
+            for ftype in new_producers:
+                new_supp = self._create_local_copy(
+                    old_suppliers[ftype], region)
+
+                new_supp["exchanges"] = [{
+                    "amount": data.loc[region, suptype].values.item(),
+                    "name": new_producers[ftype][suptype]["name"],
+                    "location": new_producers[ftype][suptype]["location"],
+                    "unit": "kilogram",
+                    "type": "technosphere",
+                    "reference product": new_producers[ftype][suptype]["reference product"],
+                    "product": new_producers[ftype][suptype]["reference product"]
+                } for suptype in new_producers[ftype]]
+
+                new_supp["exchanges"].append({
+                    "amount": 1,
+                    "name": new_supp["name"],
+                    "location": region,
+                    "unit": "kilogram",
+                    "type": "production",
+                    "reference product": "fuel",
+                    "product": "fuel"
+                })
+
+                self.db.append(new_supp)
+
+            shortcuts = {
+                "diesel": "EV-d",
+                "gasoline": "EV-p"
+            }
+
+            for ftype in shortcuts:
+                # diesel cars
+                cars = list(ws.get_many(
+                    icevs, ws.contains("name", shortcuts[ftype])))
+                for car in cars:
+                    # some local activities might already exist
+                    local_dcar = self._get_local_act_or_copy(
+                        cars, car, region)
+                    # replace diesel supplier
+                    fuel_ex = next(ws.technosphere(
+                        local_dcar,
+                        ws.startswith(
+                            "name",
+                            "fuel supply for {} vehicles".format(ftype))))
+                    fuel_ex["location"] = region
+
+    def _get_local_act_or_copy(self, db, act, region):
+        """
+        Find and return a local activity. If it is not found,
+        create a local copy, append it to the database and return it.
+        If multiple results are found, throw a ValueError.
+        """
+        local_acts = list(ws.get_many(
+            db,
+            ws.equals("name", act["name"]),
+            ws.equals("location", region)))
+        if len(local_acts) == 1:
+            return local_acts[0]
+        elif len(local_acts) == 0:
+            new_act = self._create_local_copy(act, region)
+            self.db.append(new_act)
+            return new_act
+        else:
+            raise ValueError("Multiple activities found for {} in {}"
+                             .format(act["name"], region))
