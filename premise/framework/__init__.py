e95fe7636e72f0b177a74a87fadf2c1a167e235e
Benjamin Fuchs <benjamin.fuchs@dlr.de>
2021-11-19 10:27:21 +0100

    implements logic operations based on classes for delayed execution

---
+++ premise/framework/__init__.py
@@ -0,0 +1,60 @@
+﻿
+
+class LogOps:
+    """
+    This class serves as an encapsulation for logical operations based 
+    on selectors (arrays of True False values) which are under a
+    delayed execution.
+    """
+
+    def __init__(self, a, b, kind):
+        if kind == "and":
+            self._expr = lambda df: a(df) * b(df)
+            self._repr = f"{a} and {b}"
+        elif kind == "or":
+            self._expr = lambda df: a(df) + b(df)
+            self._repr = f"{a} or {b}"
+        else:
+            raise ArithmeticError("no bool operator of or or and")
+
+    def __call__(self, df):
+        return self._expr(df)
+
+    def __and__(self, other):
+        return self.__class__(self, other, "and")
+
+    def __or__(self, other):
+        return self.__class__(self, other, "or")
+
+    def __repr__(self):
+        return self._repr
+
+    def __bool__(self):
+        raise ArithmeticError("do use | or &. 'and' and 'or' are not supported!")
+
+
+class BasicOperation:
+    """
+
+    """
+
+    def __call__(self, df):
+        return self._selector(df)
+
+    def __or__(self, other):
+        return LogOps(self, other, "or")
+
+    def __and__(self, other):
+        return LogOps(self, other, "and")
+
+    def __ior__(self, other):
+        return LogOps(self, other, "or")
+
+    def __iand__(self, other):
+        return LogOps(self, other, "and")
+
+    def __repr__(self):
+        return str(self._repr)
+
+    def __bool__(self):
+        raise ArithmeticError("do use | or &. and and or are not supported!")
---
+++ premise/framework/logics.py
@@ -0,0 +1,13 @@
+﻿from premise.framework import BasicOperation
+
+
+class contains(BasicOperation):
+    def __init__(self, key, val):
+        self._selector = lambda df: df[key] == val
+        self._repr = f"<filter df[{key}] == {val}>"
+
+
+class excludes(BasicOperation):
+    def __init__(self, key, val):
+        self._selector = lambda df: df[key] != val
+        self._repr = f"<filter df[{key}] != {val}>"
