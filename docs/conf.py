c4f4b38a4c6a94615ae5aaa9a54f28cbfa80274c
romainsacchi <r_s@me.com>
2022-01-20 19:34:58 +0100

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -13,7 +13,7 @@
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath("../premise"))
+sys.path.insert(0, os.path.abspath("../.."))
 
 # -- Project information -----------------------------------------------------
 
