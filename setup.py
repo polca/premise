49d5ff836914ee37f6e97dbeae6b27250e956886
romainsacchi <r_s@me.com>
2022-02-20 10:33:05 +0100

    Version bump

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ def package_files(directory):
 
 setup(
     name="premise",
-    version="1.0.2",
+    version="1.0.3",
     packages=packages,
     author="Romain Sacchi <romain.sacchi@psi.ch>, Alois Dirnaichner <dirnaichner@pik-potsdam.de>, Chris Mutel <chris.mutel@psi.ch>",
     license=open("LICENSE").read(),
