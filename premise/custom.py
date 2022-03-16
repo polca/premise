cbcf4ffe2ffd881788f50b6152e592e620dfaca3
romainsacchi <r_s@me.com>
2022-03-16 20:19:31 +0000

    Black reformating

--- premise/custom.py
+++ premise/custom.py
@@ -105,7 +105,8 @@ def check_inventories(custom_scenario, data, model, pathway, custom_data):
                                         for e in a["exchanges"]
                                         if e["type"] == flow_type
                                         and any(
-                                            i.lower() in e["name"].lower() for i in items_to_include
+                                            i.lower() in e["name"].lower()
+                                            for i in items_to_include
                                         )
                                     ]
                     if "replaces" in v:
