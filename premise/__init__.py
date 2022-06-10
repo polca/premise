b61784132af81296da6123426d3c869609a7bf92
romainsacchi <r_s@me.com>
2022-06-10 18:01:56 +0200

    Fix linking with new CNG datasets

--- premise/__init__.py
+++ premise/__init__.py
@@ -1,5 +1,5 @@
 __all__ = ("NewDatabase", "clear_cache")
-__version__ = (1, 1, 7)
+__version__ = (1, 1, 8)
 
 from pathlib import Path
 
--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ def package_files(directory):
 
 setup(
     name="premise",
-    version="1.1.7",
+    version="1.1.8",
     python_requires=">=3.9",
     packages=packages,
     author="Romain Sacchi <romain.sacchi@psi.ch>, Alois Dirnaichner <dirnaichner@pik-potsdam.de>, Chris Mutel <chris.mutel@psi.ch>",
