a6681c4794c65422f92292b8261fb42d52ec1a37
romainsacchi <r_s@me.com>
2022-07-17 18:37:03 +0200

    Fix filepath when exporting to Simapro

--- premise/export.py
+++ premise/export.py
@@ -4,6 +4,7 @@ import json
 import os
 import re
 from typing import Dict
+from pathlib import Path
 
 import yaml
 
@@ -588,7 +589,7 @@ class Export:
         dict_refs = load_references()
 
         with open(
-            self.filepath / filename, "w", newline="", encoding="latin1"
+            Path(self.filepath / filename), "w", newline="", encoding="latin1"
         ) as csvFile:
             writer = csv.writer(csvFile, delimiter=";")
             for item in headers:
