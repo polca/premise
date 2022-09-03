e07af4e7ee2230f67f08d67f5c43108551f4db77
romainsacchi <r_s@me.com>
2022-09-03 10:08:28 +0000

    Black reformating

--- setup.py
+++ setup.py
@@ -53,7 +53,7 @@ setup(
         "cryptography",
         "premise_gwp",
         "pyYaml",
-        "sparse"
+        "sparse",
     ],
     url="https://github.com/polca/premise",
     description="Coupling IAM output to ecoinvent LCA database ecoinvent for prospective LCA",
