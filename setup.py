f815021b742cd432a5012a9093fa4424b86437e4
romainsacchi <r_s@me.com>
2023-03-25 14:26:45 +0100

    Version bump

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ def package_files(directory):
 
 setup(
     name="premise",
-    version="1.5.0-alpha",
+    version="1.5.0-alpha.1",
     python_requires=">=3.9,<3.11",
     packages=packages,
     author="Romain Sacchi <romain.sacchi@psi.ch>, Alois Dirnaichner <dirnaichner@pik-potsdam.de>, Chris Mutel "
