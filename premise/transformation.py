fb53e6ba9cea8d06d667eefe0e335cf8e814383b
romainsacchi <r_s@me.com>
2023-09-25 07:01:13 +0000

    Black reformating

--- premise/transformation.py
+++ premise/transformation.py
@@ -707,9 +707,9 @@ class BaseTransformation:
             self.database,
             ws.equals("name", name),
             ws.contains("reference product", ref_prod),
-            ws.exclude(ws.either(
-                *[ws.equals("location", loc) for loc in self.regions]
-            ))
+            ws.exclude(
+                ws.either(*[ws.equals("location", loc) for loc in self.regions])
+            ),
         )
 
         for existing_ds in existing_datasets:
