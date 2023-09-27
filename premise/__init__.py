1a420a92231c8515065af2eafe10200d2a462ca3
romainsacchi <r_s@me.com>
2023-09-27 09:31:05 +0200

    Version bump

--- premise/__init__.py
+++ premise/__init__.py
@@ -1,5 +1,5 @@
 __all__ = ("NewDatabase", "clear_cache", "get_regions_definition")
-__version__ = (1, 7, 0)
+__version__ = (1, 7, 1)
 
 from pathlib import Path
 
--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ def package_files(directory):
 
 setup(
     name="premise",
-    version="1.7.0",
+    version="1.7.1",
     python_requires=">=3.9,<3.11",
     packages=packages,
     author="Romain Sacchi <romain.sacchi@psi.ch>, Alois Dirnaichner <dirnaichner@pik-potsdam.de>, Chris Mutel "
