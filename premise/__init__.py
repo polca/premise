35d0262891d9a8b926f4bdbd3f1a31ed03c0b042
romainsacchi <r_s@me.com>
2024-01-26 14:27:02 +0100

    Version bump

--- premise/__init__.py
+++ premise/__init__.py
@@ -1,5 +1,5 @@
 __all__ = ("NewDatabase", "clear_cache", "get_regions_definition")
-__version__ = (1, 8, 2, "dev6")
+__version__ = (1, 8, 2, "dev7")
 
 
 from .ecoinvent_modification import NewDatabase
