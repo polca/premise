fc8387bccce59470a5bd0cad470e9a5229300652
romainsacchi <r_s@me.com>
2024-03-14 19:21:07 +0000

    Black reformating

--- premise/transformation.py
+++ premise/transformation.py
@@ -472,7 +472,10 @@ class BaseTransformation:
         parameters = dataset["parameters"]
         possibles = ["efficiency", "efficiency_oil_country", "efficiency_electrical"]
 
-        if dataset["name"] == "electricity production, at lignite-fired IGCC power plant, pre, pipeline 200km, storage 1000m":
+        if (
+            dataset["name"]
+            == "electricity production, at lignite-fired IGCC power plant, pre, pipeline 200km, storage 1000m"
+        ):
             print(old_ei_eff, new_eff, dataset["location"])
 
         if any(i in dataset for i in possibles):
@@ -579,7 +582,13 @@ class BaseTransformation:
             current_efficiency = 1
 
         if energy_input == 0:
-            print(dataset["name"], dataset["location"], current_efficiency, energy_input, energy_out)
+            print(
+                dataset["name"],
+                dataset["location"],
+                current_efficiency,
+                energy_input,
+                energy_out,
+            )
             print(fuel_filters)
 
         if "parameters" in dataset:
