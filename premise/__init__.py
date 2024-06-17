8eca7e9e2a7319db97254e79520e4e114c4c5320
romainsacchi <romain@Romains-iMac.psi.ch>
2024-06-17 12:17:12 +0200

    Fix test

--- premise/__init__.py
+++ premise/__init__.py
@@ -4,7 +4,7 @@ __all__ = (
     "clear_inventory_cache",
     "get_regions_definition",
 )
-__version__ = (2, 1, 1, "dev0")
+__version__ = (2, 1, 1, "dev1")
 
 
 from premise.new_database import NewDatabase
