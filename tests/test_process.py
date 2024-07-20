6be6909535c817d99f1cfa2f3c15d03cc5fbd533
romainsacchi <r_s@me.com>
2024-07-20 12:18:07 +0000

    Black reformating

--- tests/test_process.py
+++ tests/test_process.py
@@ -1,4 +1,5 @@
 import os
+
 import bw2io
 
 ei_user = os.environ["EI_USERNAME"]
@@ -8,11 +9,12 @@ bw2io.projects.set_current("ei")
 
 bw2io.import_ecoinvent_release(
     version="3.10",
-    system_model="cutoff", # other options are "consequential", "apos" and "EN15804"
+    system_model="cutoff",  # other options are "consequential", "apos" and "EN15804"
     username=ei_user,
     password=ei_pass,
-    biosphere_name="biosphere"
+    biosphere_name="biosphere",
 )
+
 
 def test_presence_database():
     assert "ecoinvent-3.10-cutoff" in bw2io.databases
