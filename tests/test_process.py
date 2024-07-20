28d0f85bd4107b784c5664040a21be79ac8e4f7f
romainsacchi <r_s@me.com>
2024-07-20 14:24:25 +0200

    Update docs on batteries.

--- tests/test_process.py
+++ tests/test_process.py
@@ -1,14 +1,15 @@
 import os
-import bw2io
+import bw2io, bw2data
 
 ei_user = os.environ["EI_USERNAME"]
 ei_pass = os.environ["EI_PASSWORD"]
 
-bw2io.projects.set_current("ei")
+bw2data.projects.set_current("ei")
 
 bw2io.import_ecoinvent_release(
     version="3.10",
-    system_model="cutoff", # other options are "consequential", "apos" and "EN15804"
+    # other options are "consequential", "apos" and "EN15804"
+    system_model="cutoff",
     username=ei_user,
     password=ei_pass,
     biosphere_name="biosphere"
@@ -17,3 +18,20 @@ bw2io.import_ecoinvent_release(
 def test_presence_database():
     assert "ecoinvent-3.10-cutoff" in bw2io.databases
     print(bw2io.databases)
+
+def test_brightway():
+    for ei_version in ["3.8", "3.9.1", "3.10"]:
+        for system_model in ["cutoff", "consequential", "apos", "EN15804"]:
+
+            bw2data.projects.set_current(f"ecoinvent-{ei_version}-{system_model}")
+
+            if f"ecoinvent-{ei_version}-cutoff" not in bw2data.databases:
+                bw2io.import_ecoinvent_release(
+                    version=ei_version,
+                    system_model="cutoff",
+                    username=ei_user,
+                    password=ei_pass,
+                )
+
+                print(bw2data.projects.current)
+                print(bw2data.databases)
