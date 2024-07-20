e9a1c35a9d3b7760cf2ab8fb461dd53c7e0a833a
romainsacchi <r_s@me.com>
2024-07-20 14:53:19 +0200

    Update docs on batteries.

--- tests/test_process.py
+++ tests/test_process.py
@@ -22,103 +22,87 @@ def test_brightway():
             if f"ecoinvent-{ei_version}-{system_model}" not in bw2data.databases:
                 bw2io.import_ecoinvent_release(
                     version=ei_version,
-                    system_model="cutoff",
+                    system_model=system_model,
                     username=ei_user,
                     password=ei_pass,
                 )
 
-                ndb = NewDatabase(
-                    scenarios=scenarios,
-                    source_db=f"ecoinvent-{ei_version}-cutoff",
-                    source_version=ei_version,
-                    key=key,
-                    system_model=system_model,
-                    biosphere_name=f"ecoinvent-{ei_version}-biosphere",
-                )
+            ndb = NewDatabase(
+                scenarios=scenarios,
+                source_db=f"ecoinvent-{ei_version}-cutoff",
+                source_version=ei_version,
+                key=key,
+                system_model=system_model,
+                biosphere_name=f"ecoinvent-{ei_version}-biosphere",
+            )
 
-                ndb.update()
+            ndb.update()
 
-                ndb.write_db_to_brightway(
-                    [
-                        "test1",
-                        "test2",
-                    ]
-                )
+            ndb.write_db_to_brightway(
+                [
+                    "test1",
+                    "test2",
+                ]
+            )
 
-                method = [
-                    m for m in bw2data.methods if "IPCC" in m[0]
-                ][0]
+            method = [
+                m for m in bw2data.methods if "IPCC" in m[0]
+            ][0]
 
-                lca = bw2calc.LCA(
-                    {bw2data.Database("test1").random(): 1},
-                    method
-                )
-                lca.lci()
-                lca.lcia()
-                assert isinstance(lca.score, float)
-                print(lca.score)
+            lca = bw2calc.LCA(
+                {bw2data.Database("test1").random(): 1},
+                method
+            )
+            lca.lci()
+            lca.lcia()
+            assert isinstance(lca.score, float)
+            print(lca.score)
 
 def test_superstructure():
     for ei_version in ["3.8", "3.9.1", "3.10"]:
         for system_model in ["cutoff", "consequential", "apos", "EN15804"]:
             bw2data.projects.set_current(f"ecoinvent-{ei_version}-{system_model}")
 
-            if f"ecoinvent-{ei_version}-{system_model}" not in bw2data.databases:
-                bw2io.import_ecoinvent_release(
-                    version=ei_version,
-                    system_model="cutoff",
-                    username=ei_user,
-                    password=ei_pass,
-                )
+            ndb = NewDatabase(
+                scenarios=scenarios,
+                source_db=f"ecoinvent-{ei_version}-cutoff",
+                source_version=ei_version,
+                key=key,
+                system_model=system_model,
+                biosphere_name=f"ecoinvent-{ei_version}-biosphere",
+            )
 
-                ndb = NewDatabase(
-                    scenarios=scenarios,
-                    source_db=f"ecoinvent-{ei_version}-cutoff",
-                    source_version=ei_version,
-                    key=key,
-                    system_model=system_model,
-                    biosphere_name=f"ecoinvent-{ei_version}-biosphere",
-                )
+            ndb.update()
 
-                ndb.update()
+            ndb.write_superstructure_db_to_brightway(name="super_test")
 
-                ndb.write_superstructure_db_to_brightway(name="super_test")
+            method = [
+                m for m in bw2data.methods if "IPCC" in m[0]
+            ][0]
 
-                method = [
-                    m for m in bw2data.methods if "IPCC" in m[0]
-                ][0]
-
-                lca = bw2calc.LCA(
-                    {bw2data.Database("super_test").random(): 1},
-                    method
-                )
-                lca.lci()
-                lca.lcia()
-                assert isinstance(lca.score, float)
-                print(lca.score)
+            lca = bw2calc.LCA(
+                {bw2data.Database("super_test").random(): 1},
+                method
+            )
+            lca.lci()
+            lca.lcia()
+            assert isinstance(lca.score, float)
+            print(lca.score)
 
 def test_simapro_export():
     for ei_version in ["3.8", "3.9.1", "3.10"]:
         for system_model in ["cutoff", "consequential", "apos", "EN15804"]:
             bw2data.projects.set_current(f"ecoinvent-{ei_version}-{system_model}")
 
-            if f"ecoinvent-{ei_version}-{system_model}" not in bw2data.databases:
-                bw2io.import_ecoinvent_release(
-                    version=ei_version,
-                    system_model="cutoff",
-                    username=ei_user,
-                    password=ei_pass,
-                )
+            ndb = NewDatabase(
+                scenarios=scenarios,
+                source_db=f"ecoinvent-{ei_version}-cutoff",
+                source_version=ei_version,
+                key=key,
+                system_model=system_model,
+                biosphere_name=f"ecoinvent-{ei_version}-biosphere",
+            )
 
-                ndb = NewDatabase(
-                    scenarios=scenarios,
-                    source_db=f"ecoinvent-{ei_version}-cutoff",
-                    source_version=ei_version,
-                    key=key,
-                    system_model=system_model,
-                    biosphere_name=f"ecoinvent-{ei_version}-biosphere",
-                )
+            ndb.update()
 
-                ndb.update()
-
-                ndb.write_db_to_simapro()
+            ndb.write_db_to_simapro()
