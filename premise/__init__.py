6b2a7b55da92d9b6b912adaebd393a0492252b90
romainsacchi <r_s@me.com>
2024-08-06 11:07:25 +0200

    version bump

--- premise/__init__.py
+++ premise/__init__.py
@@ -4,7 +4,7 @@ __all__ = (
     "clear_inventory_cache",
     "get_regions_definition",
 )
-__version__ = (2, 1, 1, "dev6")
+__version__ = (2, 1, 1, "dev7")
 
 
 from premise.new_database import NewDatabase
