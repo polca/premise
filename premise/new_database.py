14d9cf082830796edf04ccc55dbd8327b3b286ab
romainsacchi <r_s@me.com>
2024-12-12 17:29:03 +0100

    Adds LCIs of biochar, enhanced weathering of rock and ocean liming.

--- premise/new_database.py
+++ premise/new_database.py
@@ -185,6 +185,9 @@ FILEPATH_RAIL_FREIGHT = INVENTORY_DIR / "lci-rail-freight.xlsx"
 FILEPATH_PV_GAAS = INVENTORY_DIR / "lci-PV-GaAs.xlsx"
 FILEPATH_PV_PEROVSKITE = INVENTORY_DIR / "lci-PV-perovskite.xlsx"
 FILEPATH_BATTERY_CAPACITY = INVENTORY_DIR / "lci-battery-capacity.xlsx"
+FILEPATH_BIOCHAR = INVENTORY_DIR / "lci-biochar-spruce.xlsx"
+FILEPATH_ENHANCED_WEATHERING = INVENTORY_DIR / "lci-coastal-enhanced-weathering.xlsx"
+FILEPATH_OCEAN_LIMING = INVENTORY_DIR / "lci-ocean-liming.xlsx"
 
 config = load_constants()
 
@@ -793,6 +796,9 @@ class NewDatabase:
             (FILEPATH_RAIL_FREIGHT, "3.9"),
             (FILEPATH_PV_GAAS, "3.10"),
             (FILEPATH_PV_PEROVSKITE, "3.10"),
+            (FILEPATH_BIOCHAR, "3.10"),
+            (FILEPATH_OCEAN_LIMING, "3.10"),
+            (FILEPATH_ENHANCED_WEATHERING, "3.10"),
         ]
         for filepath in filepaths:
             # make an exception for FILEPATH_OIL_GAS_INVENTORIES
