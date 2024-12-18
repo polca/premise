b6e9794f696769c62b9463a37852f25a0fa411dd
romainsacchi <r_s@me.com>
2024-12-18 11:43:59 +0100

    Fix conda_build_config.yaml

--- conda/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-if [[ $PKG_NAME == "premise-bw25" ]]; then
-    python -m pip install --no-deps --ignore-installed .[bw25]
-elif [[ $PKG_NAME == "premise-bw2" ]]; then
-    python -m pip install --no-deps --ignore-installed .[bw2]
-else
-    python -m pip install --no-deps --ignore-installed .
-fi
