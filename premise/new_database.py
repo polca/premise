035df92dd76eb0edcaa35356c35892cf034fc332
dbantje <david.bantje@pik-potsdam.de>
2025-09-15 14:49:01 +0200

    Enable choice of internventions

--- premise/new_database.py
+++ premise/new_database.py
@@ -945,7 +945,11 @@ class NewDatabase:
 
         return data
 
-    def update(self, sectors: [str, list, None] = None) -> None:
+    def update(
+        self,
+        sectors: [str, list, None] = None,
+        which_interventions=["tailings", "brake_wear", "slag", "copper"],
+    ) -> None:
         """
         Update a specific sector by name.
         """
@@ -974,7 +978,7 @@ class NewDatabase:
             },
             "interventions": {
                 "func": _update_interventions,
-                "args": (self.version, self.system_model),
+                "args": (self.version, self.system_model, which_interventions),
             },
             "heat": {"func": _update_heat, "args": (self.version, self.system_model)},
             "cdr": {"func": _update_cdr, "args": (self.version, self.system_model)},
