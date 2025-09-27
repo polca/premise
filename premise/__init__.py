b4d9fe8ce216175c767768f19feb03018ec31f7e
romainsacchi <r_s@me.com>
2025-09-27 20:52:56 +0200

    version increment

--- premise/__init__.py
+++ premise/__init__.py
@@ -6,7 +6,7 @@ __all__ = (
     "clear_inventory_cache",
     "get_regions_definition",
 )
-__version__ = (2, 3, 0, "dev1")
+__version__ = (2, 3, 0)
 
 
 from premise.new_database import NewDatabase
