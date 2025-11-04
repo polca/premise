8b1e7401e44b558982ec4be7d7bece4a0ec1839a
briangentry98 <Brian.Gentry@nrel.gov>
2025-11-04 12:15:17 -0700

    update CDR query with final energy use

--- gcam/iamc_template/iamctemplatecreator_carbon_dioxide_removal.py
+++ gcam/iamc_template/iamctemplatecreator_carbon_dioxide_removal.py
@@ -9,11 +9,12 @@ def run_cdr(scenario_name):
     # Need to change path for each scenario
     DATA_DIR = Path(os.path.join('..', 'queries', 'queryresults', scenario_name))
 
-    # load LCI data from GCAM for CDR. one file: 
+    # load LCI data from GCAM for CDR. two files: 
     cdr = pd.read_csv(DATA_DIR /'co2 sequestration.csv')
+    cdr_energy = pd.read_csv(DATA_DIR /'cdr final energy.csv')
 
     # we need to reshape all of the data in a format premise can understand
-    # first, reshape CDR
+    # first, reshape cdr
     # store in temp_df
     temp_df = cdr.copy()
     temp_df['technology'] = temp_df['technology'].replace({
@@ -30,25 +31,46 @@ def run_cdr(scenario_name):
     # concatenate dfs
     cdr = pd.concat([cdr, temp_df], axis=0)
 
+    # now reshape cdr_energy
+    temp_df = cdr_energy.copy()
+    temp_df['technology'] = temp_df['technology'].replace({
+      'hightemp DAC NG': 'Solvent',
+      'hightemp DAC elec': 'Solvent',
+      'lowtemp DAC heatpump': 'Sorbent'
+    })
+
+    temp_df = temp_df.groupby(['Units', 'scenario', 'region', 'sector', 'subsector', 'technology','Year'])['value'].agg('sum').reset_index()
+    cdr_energy = temp_df.copy()
+    # add world region
+    temp_df = temp_df.groupby(['Units', 'scenario', 'sector', 'subsector', 'technology','Year'])['value'].agg('sum').reset_index()
+    temp_df['region'] = 'World'
+    cdr_energy = pd.concat([cdr_energy, temp_df], axis=0)
+
     # now we need to format these dfs into IAMC format
     # first, rename existing columns to columns in IAMC format
     cdr = cdr.rename(columns={'region': 'Region', 'scenario': 'Scenario', 'Units': 'Unit'})
+    cdr_energy = cdr_energy.rename(columns={'region': 'Region', 'scenario': 'Scenario', 'Units': 'Unit'})
 
     # replace Scenario with scenario_name
     cdr['Scenario'] = scenario_name
+    cdr_energy['Scenario'] = scenario_name
 
     # add GCAM as Model
     cdr['Model'] = 'GCAM'
+    cdr_energy['Model'] = 'GCAM'
 
-    # replace Unit column with expected values (MtC/yr)
+    # replace Unit column with expected values (MtC/yr, EJ/yr)
     cdr['Unit'] = 'MtC/yr'
+    cdr_energy['Unit'] = 'EJ/yr'
 
     # define variable
     # 
     cdr['Variable'] = 'Carbon Sequestration|Direct Air Capture|' + cdr['technology']
+    cdr_energy['Variable'] = 'Final Energy|Carbon Management|Direct Air Capture|' + cdr_energy['technology']
 
     # reorder columns and remove unnecessary columns (sector, subsector, technology)
     cdr = cdr[['Scenario', 'Region', 'Model', 'Variable', 'Unit', 'Year', 'value']]
+    cdr_energy = cdr_energy[['Scenario', 'Region', 'Model', 'Variable', 'Unit', 'Year', 'value']]
 
     # pivot dfs, creating columns for each year
 
@@ -57,8 +79,13 @@ def run_cdr(scenario_name):
                                         index=['Scenario', 'Region', 'Model', 'Variable', 'Unit'],
                                         columns=['Year'],
                                         aggfunc='sum').reset_index()
-    out_df = cdr_pivot.reset_index(drop=True)
-    out_df = pd.concat([cdr_pivot]).reset_index(drop=True)
+    cdr_energy_pivot = pd.pivot_table(cdr_energy,
+                                        values=['value'],
+                                        index=['Scenario', 'Region', 'Model', 'Variable', 'Unit'],
+                                        columns=['Year'],
+                                        aggfunc='sum').reset_index()
+    print(cdr_energy_pivot)
+    out_df = pd.concat([cdr_pivot, cdr_energy_pivot]).reset_index(drop=True)
 
     # tidy up dataframe (fix multiple index column names in year columns)
     out_df.columns = ['Scenario', 'Region', 'Model', 'Variable', 'Unit'] + [str(x[1]) for x in out_df.columns[5:]]
