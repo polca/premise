5d7370a4a38fc5b31dcf4b76fb26c25f68ba4dcf
romainsacchi <romain@Romains-iMac.psi.ch>
2026-02-13 14:50:04 +0100

    add more meaningful error message when file not found. addresses #280

--- premise/data_collection.py
+++ premise/data_collection.py
@@ -1232,7 +1232,8 @@ class IAMDataCollection:
         if file_path is None:
             if key is None:
                 raise FileNotFoundError(
-                    f"File {file_name} not found with any supported extension in {filedir}"
+                    f"Either 1) the file {file_name} cannot found with any supported extension in {filedir}"
+                    f"or 2) no decryption key provided to download the file from Zenodo. Please provide a decryption key or place the file in the specified directory."
                 )
             else:
                 # If key is provided, download the file
